c4ea1d1585afcb3b09cf96f410c6e12d69d04f6e
RENATO SILVA DE OLIVEIRA <26210937@dsn127m1246734.cfp127aluno.local>
2026-09-18 14:09:03 -0300

    criando o codigo

---
+++ 05.py
@@ -0,0 +1,11 @@
+def somar():
+  numero1 = float(input("digite um numero: "))
+  numero2 = float(input("digite um numero: "))
+  resultado = numero1 + numero2
+  print (resultado)
+
+  while True:
+    print("calculadora")
+    print("1-Adição")
+    print("2- subtração")
+    print()
---
+++ formas.py
@@ -0,0 +1,24 @@
+def circulo():
+    raio = float(input("qual valor do raio: "))
+    resultado = 3.14 (raio * raio)
+    print (f"a area do circulo é {resultado}")
+def triangulo():
+    base1 = float(input("qual valor da base: "))
+    altura = float(input("qual valor da altura: "))
+    resultado = (base1 * altura /2)
+    print (f")
+while True:
+    print("Calculadora de areas")
+    print("1 - circulo")
+    print("2 - triangulo")
+    print("3 - quadrado")
+    print("4 - retangulo")
+    print("5 - paralelogramo")
+    print("6 - losangulo")
+    print("7 - trapezio")
+    print("0 - sair do programa")
+
+    opcao = input("digite um numero")
+    
+    if opcao == '1':
+ 
